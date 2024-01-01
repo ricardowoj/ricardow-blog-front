4cb5d50f1391c436ed1d15a0ab05f66155ffe529
Ricardo <ricardowoj@hotmail.com>
2024-01-01 18:00:26 +0000

    change project to quasar framework

--- src/profiles/profile-apis.js
+++
@@ -1,8 +0,0 @@
-const urlApi = Object.freeze({
-  RICARDOW_BLOG_API: {
-    path: "/ricardow-blog-api/",
-    port: 8181
-  }
-})
-
-export default urlApi;
--- src/profiles/profile-list.js
+++
@@ -1,6 +0,0 @@
-const PROFILE_LIST = {
-  PROD: "prod",
-  DEV: "dev"
-}
-
-export default PROFILE_LIST;
